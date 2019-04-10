c3abd5ea03b3799ac0d7c197626dbc281615110b
Arthur Flôr <arthurflor23@gmail.com>
2019-04-10 17:24:03 -0300

    Update python modules

--- custom_packages.sh
+++ custom_packages.sh
@@ -24,18 +24,14 @@ desktop=$(echo $DESKTOP_SESSION | grep -Eo "plasma|gnome")
 
 ### Common packages 
     pikaur -S wd719x-firmware aic94xx-firmware --noconfirm
-    pikaur -S jre multibootusb keepassxc pdfarranger --noconfirm
-
     pikaur -S google-chrome qbittorrent gimp vlc --noconfirm
+
+    pikaur -S jre visual-studio-code-bin multibootusb keepassxc pdfarranger --noconfirm
     pikaur -S virtualbox virtualbox-guest-iso virtualbox-ext-oracle --noconfirm
 
     pikaur -S libreoffice-{fresh,extension-languagetool} hunspell-en_US hunspell-pt-br --noconfirm
     echo -e "export SAL_USE_VCLPLUGIN=gtk3" | sudo tee --append /etc/profile.d/libreoffice-fresh.sh
     sudo sed -i 's/Logo=1/Logo=0/' /etc/libreoffice/sofficerc
-
-    pikaur -S smartgit visual-studio-code-bin --noconfirm
-    pikaur -S opencv python-{matplotlib,numpy,pylint,tensorflow-opt} --noconfirm
-    # vtk glew
 
 ### Games
     cd ~ && wget -c https://download.wakfu.com/full/linux/x64 -O - | tar -xz
