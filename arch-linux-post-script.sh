e34d4e724b224900aef48d822ada64d34cc9864d
Arthur Flôr <arthurflor23@gmail.com>
2020-12-13 21:36:06 -0300

    Update Gnome feelings

--- arch-linux-post-script.sh
+++ arch-linux-post-script.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-desktop=$(echo $DESKTOP_SESSION | grep -Eo "plasma|gnome")
+# ===========================================================================
+# GNOME - SHORTCUTS
+# ===========================================================================
+
+## Hide all normal windows  : Super + D
+## Monitor                  : Ctrl + Alt + Delete (gnome-system-monitor)
+## Nautilus                 : Super + E (nautilus --new-window)
+## Print                    : Print (gnome-screenshot -a --interactive)
+## Switch to workspace      : Super + [F1, F2, F3, F4]
+## Switch windows           : Alt + Tab
+## Terminal                 : Ctrl + Alt + T (gnome-terminal)
+## Power Off                : Super + Delete (gnome-session-quit --power-off)
+
+# ===========================================================================
+# GNOME - EXTENSIONS
+# ===========================================================================
+
+## Arch Linux Updates Indicator
+## Clipboard Indicator
+## Dash to Dock
+## GSConnect
+## Sound Input & Output Device Chooser
+## Top Panel Workspace Scroll
 
 # ===============================================================================
 # SYSTEM
 # ===============================================================================
 
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-sudo sed -i 's/loglevel=3/quiet loglevel=0 rd.systemd.show_status=0 rd.udev.log_priority=0 pci=noaer fbcon=nodefer/g' /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg && sudo sed -i 's/echo/#echo/g' /boot/grub/grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i 's/echo/#echo/g' /boot/grub/grub.cfg
 
 echo -e 'en_US.UTF-8 UTF-8' | sudo tee --append /etc/locale.gen && sudo locale-gen
 echo -e 'FONT=lat0-16' | sudo tee --append /etc/vconsole.conf
@@ -17,84 +39,104 @@ sudo mkdir -p /etc/systemd/coredump.conf.d/
 echo -e '[Coredump]\nStorage=none' | sudo tee --append /etc/systemd/coredump.conf.d/custom.conf
 echo 'SystemMaxUse=50M' | sudo tee --append /etc/systemd/journald.conf
 
+# ===========================================================================
+# CLEAR PACKAGES
+# ===========================================================================
+
+yay -Rcc gnome-{backgrounds,books,boxes,calendar,characters,contacts,dictionary,documents}
+yay -Rcc gnome-{font-viewer,logs,maps,menus,music,notes,photos,shell-extensions,software,todo}
+yay -Rcc baobab epiphany rygel totem vino yelp xdg-user-dirs-gtk xdg-user-dirs tracker tracker-miners
+
+yay -Rcc xf86-video-intel network-manager-applet wireless_tools
+yay -Rcc base luit cpupower dialog sushi orca man-{pages,db}
+yay -Rcc vim xterm pavucontrol mousetweaks dleyna-server
+
+yay -Qttdq | yay -Rns -
+yay -c && yay -Scc
+
+# ===============================================================================
+# INSTALL PACKAGES
+# ===============================================================================
+
+yay -S pacman-contrib base-devel fakeroot nano openssh --needed
+yay -S zip unrar p7zip neofetch ffmpegthumbnailer xmacro
+
+yay -S ttf-ms-fonts adobe-source-han-sans-otc-fonts
+yay -S system-config-printer cups-{filters,pdf} hplip-minimal pdfarranger img2pdf
+yay -S jre-openjdk keepassxc ventoy-bin papirus-icon-theme
+
+yay -S hunspell hunspell-{en_US,pt-br} hyphen hyphen-{en,pt-br} libmythes mythes-{en,pt-br}
+yay -S libreoffice-{fresh,extension-languagetool}
+
+yay -S transmission-gtk gimp vlc geary google-chrome chrome-gnome-shell
+yay -S smartgit visual-studio-code-bin ankama-launcher
+yay -S virtualbox virtualbox-guest-iso virtualbox-ext-oracle
+
+# ===========================================================================
+# GNOME - ENVIRONMENT
+# ===========================================================================
+
+# Autostart packages
+mkdir -p ~/.config/autostart/
+echo -e "[Desktop Entry]\nType=Application\nName=transmission-gtk\nExec=transmission-gtk -m" > ~/.config/autostart/transmission-gtk.desktop
+
+# Autostart bluetooth
 sudo sed -i 's/#AutoEnable=false/AutoEnable=true/g' /etc/bluetooth/main.conf
+
+# Background images
 sudo rm -R /usr/share/backgrounds/anarchy
 
+sudo cp -R ./dynamic-wallpaper/** /usr/share/backgrounds/gnome/
+sudo mv /usr/share/backgrounds/gnome/ghib/ghib-dynamic.xml /usr/share/gnome-background-properties/
 
-# ===============================================================================
-# GNOME
-# ===============================================================================
+# Custom folders
+mkdir ~/Code ; gio set ~/Code metadata::custom-icon-name "folder-script"
+mkdir ~/VirtualBox\ VMs ; gio set ~/VirtualBox\ VMs metadata::custom-icon-name "folder-linux"
 
-if [ $desktop == 'gnome' ] ; then
+# ===========================================================================
+# GNOME - GSETTINGS
+# ===========================================================================
 
-	# ===========================================================================
-	# GNOME - SHORTCUTS
-	# ===========================================================================
+# Theme
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
 
-	## Hide all normal windows  : Super + D
-	## Monitor                  : Ctrl + Alt + Delete (gnome-system-monitor)
-	## Nautilus                 : Super + E (nautilus --new-window)
-	## Print                    : Print (gnome-screenshot -a --interactive)
-	## Switch to workspace      : Super + [F1, F2, F3, F4]
-	## Switch windows           : Alt + Tab
-	## Terminal                 : Ctrl + Alt + T (gnome-terminal)
-    ## Power Off                : Super + Delete (gnome-session-quit --power-off)
+# Mouse & Touchpad
+gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
+gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
 
-	# ===========================================================================
-	# GNOME - EXTENSIONS
-	# ===========================================================================
+gsettings set org.gnome.desktop.peripherals.mouse speed .4
+gsettings set org.gnome.desktop.peripherals.touchpad speed .4
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 
-	## Arch Linux Updates Indicator
-	## Clipboard Indicator
-	## Dash to Dock
-	## GSConnect
-	## Sound Input & Output Device Chooser
-	## Top Panel Workspace Scroll
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.mouse speed .4
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.touchpad speed .4
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 
-	# ===========================================================================
-	# GNOME - PACKAGES
-	# ===========================================================================
-	
-	yay -Rcc baobab epiphany rygel totem xdg-user-dirs-gtk vino yelp
-	yay -Rcc gnome-{books,boxes,calendar,characters,contacts,dictionary,documents,font-viewer}
-	yay -Rcc gnome-{logs,maps,music,notes,photos,shell-extensions,software,todo}
+# Keyboard
+gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.keyboard numlock-state true
 
-    yay -S ffmpegthumbnailer chrome-gnome-shell
-    yay -S geary transmission-gtk gnome-multi-writer
+# GEdit without extra blank line
+gsettings set org.gnome.gedit.preferences.editor ensure-trailing-newline false
 
-	# ===========================================================================
-	# GNOME - ENVIRONMENT
-	# ===========================================================================
+# Screencast unlimited
+gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 0s
 
-	mkdir -p ~/.config/autostart/
-	echo -e "[Desktop Entry]\nType=Application\nName=geary\nExec=geary --gapplication-service" > ~/.config/autostart/geary.desktop
-	echo -e "[Desktop Entry]\nType=Application\nName=transmission-gtk\nExec=transmission-gtk -m" > ~/.config/autostart/transmission-gtk.desktop
+# ===========================================================================
+# ACPID LID CLOSE/OPEN EVENT
+# ===========================================================================
 
-	sudo cp -R ./dynamic-wallpaper/** /usr/share/backgrounds/gnome/
-	sudo mv /usr/share/backgrounds/gnome/ghib/ghib-dynamic.xml /usr/share/gnome-background-properties/
+yay -S acpid
+sudo systemctl enable acpid
 
-    # Theme
-    gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
-    # Keyboard & Mouse
-    gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing false
-    gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
-	# GEdit without extra blank line
-	gsettings set org.gnome.gedit.preferences.editor ensure-trailing-newline false
-    # Screencast unlimited
-	gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 0   
-
-	# ===========================================================================
-	# GNOME - ACPID LID CLOSE/OPEN EVENT
-	# ===========================================================================
-
-	yay -S acpid
-	sudo systemctl enable acpid
-
-	echo 'HandleLidSwitch=ignore' | sudo tee --append /etc/systemd/logind.conf
-	echo 'HandleLidSwitchDocked=ignore' | sudo tee --append /etc/systemd/logind.conf
-	echo 'event=button/lid.*' | sudo tee --append /etc/acpi/events/lm_lid
-	echo 'action=/etc/acpi/lid.sh' | sudo tee --append /etc/acpi/events/lm_lid
-	echo -e '#!/bin/bash
+echo 'HandleLidSwitch=ignore' | sudo tee --append /etc/systemd/logind.conf
+echo 'HandleLidSwitchDocked=ignore' | sudo tee --append /etc/systemd/logind.conf
+echo 'event=button/lid.*' | sudo tee --append /etc/acpi/events/lm_lid
+echo 'action=/etc/acpi/lid.sh' | sudo tee --append /etc/acpi/events/lm_lid
+echo -e '#!/bin/bash
 
 user=$(ps -o uname= -p $(pgrep "^gnome-shell$"))
 screen=$(cat /sys/class/drm/card0/*HDMI*/status | grep "^connected" | wc -l)
@@ -111,57 +153,10 @@ if [ $? = 0 ]; then
     runuser -l $user -c "busctl --user set-property org.gnome.Mutter.DisplayConfig /org/gnome/Mutter/DisplayConfig org.gnome.Mutter.DisplayConfig PowerSaveMode i 0"
 fi' > /etc/acpi/lid.sh
 
-	chmod +x /etc/acpi/lid.sh
-
-
-# ===============================================================================
-# KDE PLASMA
-# ===============================================================================
-
-elif [ $desktop == 'plasma' ] ; then
-    
-	# ===========================================================================
-	# KDE PLASMA - PACKAGES
-	# ===========================================================================
-
-	yay -S kde-gtk-config kdeplasma-addons kinfocenter sddm-kcm user-manager
-	yay -S discover packagekit-qt5 bluedevil ffmpegthumbs kdegraphics-thumbnailers
-	yay -S breeze-gtk plasma-browser-integration kdeconnect spectacle print-manager
-	yay -S ark okular gwenview skanlite kate kcalc filelight partitionmanager
-	yay -S multibootusb qbittorrent
-
-	# ===========================================================================
-	# KDE PLASMA - ENVIRONMENT
-	# ===========================================================================
-
-	echo -e '[Wallet]\nEnabled=false' | sudo tee --append ~/.config/kwalletrc
-
-fi
-
+chmod +x /etc/acpi/lid.sh
 
 # ===============================================================================
-# PACKAGES
-# ===============================================================================
-
-yay -Rcc vim xterm pavucontrol
-
-yay -S pacman-contrib base-devel fakeroot --needed
-yay -S nano openssh neofetch xmacro zip unrar p7zip
-
-yay -S system-config-printer cups-{filters,pdf} hplip-minimal
-yay -S jre-openjdk keepassxc pdfarranger img2pdf
-
-yay -S ttf-ms-fonts adobe-source-han-sans-otc-fonts
-yay -S libreoffice-{fresh,extension-languagetool}
-yay -S hunspell-{en_US,pt-br} hyphen-{en,pt-br} libmythes mythes-{en,pt-br}
-
-yay -S virtualbox virtualbox-guest-iso virtualbox-ext-oracle
-yay -S google-chrome gimp vlc ankama-launcher
-yay -S smartgit visual-studio-code-bin
-
-
-# ===============================================================================
-# ENVIRONMENT
+# ENVIRONMENT SCRIPTS
 # ===============================================================================
 
 sudo gpasswd -a $(whoami) games
@@ -190,5 +185,3 @@ macroplay () {
   for ((;;)) do xmacroplay < "$1"; done
 }
 ' >> ~/.bashrc
-
-yay -c && yay -Scc
