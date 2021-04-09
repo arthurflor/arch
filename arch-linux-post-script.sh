8fb6e2ffecdfafa3d5dc64b6b4c932a6d18f241b
Arthur Flôr <arthurflor23@gmail.com>
2021-04-08 23:15:37 -0300

    Remove clocks

--- arch-linux-post-script.sh
+++ arch-linux-post-script.sh
@@ -26,8 +26,8 @@
 # CLEAR PACKAGES
 # ===========================================================================
 
-yay -Rcc gnome-{backgrounds,books,boxes,characters,contacts,dictionary,documents,font-viewer}
-yay -Rcc gnome-{logs,maps,menus,music,notes,photos,shell-extensions,software,todo}
+yay -Rcc gnome-{backgrounds,books,boxes,characters,clocks,contacts,dictionary,documents}
+yay -Rcc gnome-{font-viewer,logs,maps,menus,music,notes,photos,shell-extensions,software,todo}
 yay -Rcc epiphany evolution totem rygel tracker tracker-miners vino yelp xdg-user-dirs-gtk xdg-user-dirs
 
 yay -Rcc xf86-video-intel cpupower network-manager-applet wireless_tools
